acf5e92f77ab51e3be4b4a8100fca22735e526e5
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2021-10-08 22:36:00 +0900

    Initial commit

---
+++ ArticleApp+Rx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArticleApp+Rx
+//
+//  Created by Taisei Sakamoto on 2021/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
