eb14c6a7245da276ca2ecc329a21216788fe6183
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2021-10-09 14:27:00 +0900

    Autolayoutを実装

---
+++ ArticleApp+Rx/Controllers/ArticleViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ArticleViewController.swift
+//  ArticleApp+Rx
+//
+//  Created by Taisei Sakamoto on 2021/10/08.
+//
+
+import UIKit
+
+class ArticleViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Helpers
+}
